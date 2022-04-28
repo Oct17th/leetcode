12f05dbe40800022b5db967afb9b686e7bd7ef8d
yijie <736842589@qq.com>
2022-04-28 16:01:26 +0800

    feat:二叉树的中序遍历

---
+++ editor/cn/[94]二叉树的中序遍历.java
@@ -0,0 +1,85 @@
+//package cn;
+////给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+////
+////
+////
+//// 示例 1：
+////
+////
+////输入：root = [1,null,2,3]
+////输出：[1,3,2]
+////
+////
+//// 示例 2：
+////
+////
+////输入：root = []
+////输出：[]
+////
+////
+//// 示例 3：
+////
+////
+////输入：root = [1]
+////输出：[1]
+////
+////
+////
+////
+//// 提示：
+////
+////
+//// 树中节点数目在范围 [0, 100] 内
+//// -100 <= Node.val <= 100
+////
+////
+////
+////
+//// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+//// Related Topics 栈 树 深度优先搜索 二叉树 👍 1403 👎 0
+//
+//
+////leetcode submit region begin(Prohibit modification and deletion)
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Definition for a binary tree node.
+// * public class TreeNode {
+// *     int val;
+// *     TreeNode left;
+// *     TreeNode right;
+// *     TreeNode() {}
+// *     TreeNode(int val) { this.val = val; }
+// *     TreeNode(int val, TreeNode left, TreeNode right) {
+// *         this.val = val;
+// *         this.left = left;
+// *         this.right = right;
+// *     }
+// * }
+// */
+//class Solution {
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> list = new ArrayList<>();
+//        if (root == null) {
+//            return list;
+//        }
+//        inorderTraversal(list, root);
+//        return list;
+//    }
+//
+//    public void inorderTraversal(List<Integer> list, TreeNode root) {
+//        if (list == null || root == null) {
+//            return;
+//        }
+//
+//        // 左节点
+//        inorderTraversal(list, root.left);
+//        // 根节点
+//        list.add(root.val);
+//        // 右节点
+//        inorderTraversal(list, root.right);
+//    }
+//}
+////leetcode submit region end(Prohibit modification and deletion)
